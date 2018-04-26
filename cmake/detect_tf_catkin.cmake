41c259ceeb09113ff337b081147baa45c5352691
Martin Pecka <peckama2@fel.cvut.cz>
2018-04-26 19:58:20 +0200

    Added nsync as explicit include dir for tensorflow_catkin (needed in GPU build).

--- cmake/detect_tf_catkin.cmake
+++ cmake/detect_tf_catkin.cmake
@@ -6,6 +6,6 @@ if(NOT tensorflow_catkin_FOUND)
 endif()
 
 set(TENSORFLOW_FOUND 1)
-set(tensorflow_ros_INCLUDE_DIRS ${tensorflow_catkin_INCLUDE_DIRS})
+set(tensorflow_ros_INCLUDE_DIRS ${tensorflow_catkin_INCLUDE_DIRS} ${tensorflow_catkin_INCLUDE_DIRS}/external/nsync/public)
 set(tensorflow_ros_LIBRARIES ${tensorflow_catkin_LIBRARIES})
 set(tensorflow_ros_CATKIN_DEPENDS tensorflow_catkin)
