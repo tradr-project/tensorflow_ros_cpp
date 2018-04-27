bb41c56c582249936b0338768e16e3981153a272
Martin Pecka <peckama2@fel.cvut.cz>
2018-04-27 17:01:42 +0200

    Added gomp library requirement for tensorflow_catkin with CUDA >= 8.

--- cmake/detect_tf_catkin.cmake
+++ cmake/detect_tf_catkin.cmake
@@ -5,7 +5,17 @@ if(NOT tensorflow_catkin_FOUND)
   return()
 endif()
 
+message("-- -- Found tensorflow_catkin")
+
+# cuSOLVER from CUDA >= 8.0 requires OpenMP
+set(ADDITIONAL_LIBS "")
+if(NOT (${CUDA_VERSION_MAJOR} LESS 8))
+  if (CMAKE_CXX_COMPILER_ID MATCHES "GNU" )
+    set(ADDITIONAL_LIBS gomp)
+  endif()
+endif()
+
 set(TENSORFLOW_FOUND 1)
 set(tensorflow_ros_INCLUDE_DIRS ${tensorflow_catkin_INCLUDE_DIRS} ${tensorflow_catkin_INCLUDE_DIRS}/external/nsync/public)
-set(tensorflow_ros_LIBRARIES ${tensorflow_catkin_LIBRARIES})
+set(tensorflow_ros_LIBRARIES ${tensorflow_catkin_LIBRARIES} ${ADDITIONAL_LIBS})
 set(tensorflow_ros_CATKIN_DEPENDS tensorflow_catkin)
