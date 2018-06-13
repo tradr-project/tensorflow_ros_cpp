b32b09f21fe60a7b0c1a05ca7eee6d911c027ea7
Martin Pecka <peci1@seznam.cz>
2018-06-13 04:29:07 +0200

    CI: Download get-pip into /tmp

--- .travis/upgrade_pip.sh
+++ .travis/upgrade_pip.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 apt-get install curl
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python get-pip.py
+curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
+python /tmp/get-pip.py
